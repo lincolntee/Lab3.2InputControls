72f66f8f4f708e4f46fc6a6174819d9bedbe65db
lincolntee <teebh-wa14@student.tarc.edu.my>
2017-11-29 13:29:50 +0800

    Update

--- app/src/main/java/my/edu/taruc/lab32inputcontrols/MainActivity.java
+++ app/src/main/java/my/edu/taruc/lab32inputcontrols/MainActivity.java
@@ -2,12 +2,35 @@ package my.edu.taruc.lab32inputcontrols;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.CheckBox;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.Spinner;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Spinner spinnerAge;
+    private RadioGroup radioGroupGender;
+    private RadioButton radioButtonMale, radioButtonFemale;
+    private CheckBox checkBoxSmoker;
+    private TextView textViewPremium;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        //Linking UI to the program
+        spinnerAge = (Spinner)findViewById(R.id.spinnerAge);
+        radioGroupGender = (RadioGroup)findViewById(R.id.radioGroupGender);
+        radioButtonMale = (RadioButton)findViewById(R.id.radioButtonMale);
+        radioButtonFemale = (RadioButton)findViewById(R.id.radioButtonFemale);
+        checkBoxSmoker = (CheckBox)findViewById(R.id.checkBoxSmoker);
+        textViewPremium = (TextView)findViewById(R.id.textViewPremium);
+
+        //Create an adapter
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,R.array.age_group,android.R.layout.simple_spinner_item);
     }
+
 }
